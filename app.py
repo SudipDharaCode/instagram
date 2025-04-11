a9685e4bd64f29d93c2fc77e753c9aa2e0f8e153
SudipDhara <127935611+SudipDharaCode@users.noreply.github.com>
2025-04-11 14:41:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,5 +82,5 @@ async def analyze_comment(data: CommentInput):
 # Run with: uvicorn filename:app --reload
 # ---------------------------
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True) 
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True) 
 
